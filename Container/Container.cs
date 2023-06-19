b8dd454cc08fcbc78e949df87e64b6226367ac3b
Mike Weatherford <mikesweatherford@gmail.com>
2023-06-19 19:29:26 -0400

    comtainer challenge complete

--- Container/Container.cs
+++ Container/Container.cs
@@ -4,7 +4,22 @@ namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            bindings[interfaceType] = implementationType;
+        }
+
+        public T Get<T>()
+        {
+            Type interfaceType = typeof(T);
+            if (!interfaceType.IsInterface)
+                throw new ArgumentException("must be an interface.");
+
+            if (!bindings.TryGetValue(interfaceType, out Type implementationType))
+                throw new InvalidOperationException("No implementation found.");
+
+            object instance = Activator.CreateInstance(implementationType);
+            return (T)instance;
+        }
     }
 }
